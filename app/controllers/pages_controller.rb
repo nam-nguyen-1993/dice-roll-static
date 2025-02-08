0b797eecec94950e23a98a9235a3de22c06d3078
nam-nguyen-1993 <nam.nguyen@chicagobooth.edu>
2025-02-08 18:35:19 +0000

    Done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,52 @@ class PagesController < ApplicationController
 
     render({ :template => "pages_templates/home" })
   end
+
+  def roll2d6
+
+    @dice_roll = []
+    
+    2.times do
+      roll = rand(1..6)
+      @dice_roll.push(roll)
+    end
+
+    render({ :template => "pages_templates/2d6" })
+  end
+
+  def roll2d10
+
+    @dice_roll = []
+    
+    2.times do
+      roll = rand(1..10)
+      @dice_roll.push(roll)
+    end
+
+    render({ :template => "pages_templates/2d10" })
+  end
+
+  def roll1d20
+
+    @dice_roll = []
+    
+    1.times do
+      roll = rand(1..20)
+      @dice_roll.push(roll)
+    end
+
+    render({ :template => "pages_templates/1d20" })
+  end
+
+  def roll5d4
+
+    @dice_roll = []
+    
+    5.times do
+      roll = rand(1..4)
+      @dice_roll.push(roll)
+    end
+
+    render({ :template => "pages_templates/5d4" })
+  end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "pages", :action => "home"})
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
+  get("/dice/2/6", { :controller => "pages", :action => "roll2d6"})
+  get("/dice/2/10", { :controller => "pages", :action => "roll2d10"})
+  get("/dice/1/20", { :controller => "pages", :action => "roll1d20"})
+  get("/dice/5/4", { :controller => "pages", :action => "roll5d4"})
   
 end
